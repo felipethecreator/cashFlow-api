ae04df4f5c9dd5062b0a275297cddda74fc8c319
Felipe <felipinhodev@gmail.com>
2026-03-10 17:24:13 -0300

    feat(expense): adição de updatedAt na response

--- src/main/java/com/cashflow/api/expense/dto/output/ExpenseResponse.java
+++ src/main/java/com/cashflow/api/expense/dto/output/ExpenseResponse.java
@@ -16,6 +16,7 @@ public record ExpenseResponse(
         ExpensePriority priority,
         Boolean isActive,
         Boolean isRecurring,
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
 ) {
 }
