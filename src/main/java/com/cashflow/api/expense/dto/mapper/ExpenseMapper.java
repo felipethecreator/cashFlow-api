b68edadd49295ab6210f551845367913112e126b
Felipe <felipinhodev@gmail.com>
2026-03-10 17:18:39 -0300

    fix: mapeamento incorreto de categoryName e categoryId

--- src/main/java/com/cashflow/api/expense/dto/mapper/ExpenseMapper.java
+++ src/main/java/com/cashflow/api/expense/dto/mapper/ExpenseMapper.java
@@ -4,9 +4,12 @@ import com.cashflow.api.expense.dto.input.CreateExpense;
 import com.cashflow.api.expense.dto.output.ExpenseResponse;
 import com.cashflow.api.expense.entity.Expense;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ExpenseMapper {
+    @Mapping(target = "categoryName", source = "category.name")
+    @Mapping(target = "categoryId", source = "category.id")
     ExpenseResponse toDto(Expense expense);
 
     Expense toEntity(CreateExpense createExpense);
